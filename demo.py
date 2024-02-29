4f4ed68d6f59d932de77a3d8082a87d94c2da35d
ashutosh <ashutoshsinha519@gmail.com>
2024-03-01 01:31:12 +0530

    custome exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from visa_approval.logger import logging 
+from visa_approval.exception import USVisaException 
+import sys
 
-logging.info("welcome to our firts custome log")
+
+# logging.info("welcome to our firts custome log")
+try :
+    a = 1/ '1'
+except Exception as e:
+    logging.info( e)
+    raise USVisaException(e , sys) from e
